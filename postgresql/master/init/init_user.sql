ea8a4bfcc19cd7e2af1fc36da877ead1f28cfc0d
Pytws <mtavei.minin@gmail.com>
2025-01-22 19:08:06 +0300

    fix creating roles

--- postgresql/master/init/init_user.sql
+++ postgresql/master/init/init_user.sql
@@ -1,2 +1,7 @@
 CREATE USER replicator WITH REPLICATION ENCRYPTED PASSWORD 'password';
 SELECT pg_create_physical_replication_slot('replication_slot');
+
+CREATE ROLE mon_user WITH LOGIN ENCRYPTED PASSWORD 'password';
+GRANT pg_read_all_stats TO mon_user;
+
+
